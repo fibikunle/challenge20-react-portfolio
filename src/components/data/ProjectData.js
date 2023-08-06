1e598fff4e3ed209f65e8f725ebd9fb162884139
fibikunle <43515489+fibikunle@users.noreply.github.com>
2023-08-06 09:11:23 -0500

    projectdata updates

--- src/components/data/ProjectData.js
+++ src/components/data/ProjectData.js
@@ -10,58 +10,58 @@ import imgTechBlog from "../../images/imgTech.png";
 export const projects = [
   {
     title: "Tech Blog",
-    url: "https://github.com/ibikunlefemi/tech-blog",
+    url: "https://git.heroku.com/challenge14-tech-blog.git",
     image: imgTechBlog,
     description:
       "Challenge #14 for the University of Texas, Austin Coding Bootcamp 2023. The purpose of this challenge is to build a CMS-style blog site following the MVC paradigm, where developers can publish their blog posts and comment on other developers' posts as well.",
     technologies:
       "Node.js, Express.js, MySQL, Sequelize, Handlebars.js, & express-session",
-    GitHubRepo: "https://github.com/ibikunlefemi/tech-blog",
+    GitHubRepo: "https://github.com/fibikunle/challenge14-tech-blog",
   },
   {
     title: "Social Network API",
-    url: "https://github.com/ibikunlefemi/social-network-api",
+    url: "https://github.com/fibikunle/challenge18-social-network-api",
     image: imgSocialNetworkAPI,
     description:
       "Challenge #18 for the University of Texas, Austin Coding Bootcamp 2023. The purpose of this challenge is to understand the unstructured nature of NoSQL by building an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.",
     technologies: "Node.js, Express.js, MongoDB, & Mongoose",
-    GitHubRepo: "https://github.com/ibikunlefemi/nosql-social-network-app",
+    GitHubRepo: "https://github.com/fibikunle/challenge18-social-network-api",
   },
 
   {
     title: "E-Commerce Back End",
-    url: "https://github.com/ibikunlefemi/e-commerce-back-end",
+    url: "https://github.com/fibikunle/challenge13-orm-ecommerce",
     image: imgECommerceBackend,
     description:
       "Challenge #13 for the University of Texas, Austin Coding Bootcamp 2023. The purpose of this challenge is to build a back end for a generic e-commerce site using one-to-many and many-to-many database relationships.",
     technologies: "Node.js, Express.js, MySQL, & Sequelize",
-    GitHubRepo: "https://github.com/ibikunlefemi/ecommerce-backend",
+    GitHubRepo: "https://github.com/fibikunle/challenge13-orm-ecommerce",
   },
   {
     title: "Employee Tracker",
-    url: "https://github.com/ibikunlefemi/employee-tracker",
+    url: "https://github.com/fibikunle/challenge12-employee-tracker",
     image: imgEmployeeTracker,
     description:
       "Challenge #12 for the University of Texas, Austin Coding Bootcamp 2023. The purpose of this challenge is to build a command-line application to manage a company's employee database.",
     technologies: "Node.js, Inquirer v8.2.4, dotenv, & MySQL",
-    GitHubRepo: "https://github.com/ibikunlefemi/employee-tracker",
+    GitHubRepo: "https://github.com/fibikunle/challenge12-employee-tracker",
   },
   {
     title: "Note Taker",
-    url: "https://github.com/ibikunlefemi/note-taker",
+    url: "https://challenge11-note-taker-82045ac135ab.herokuapp.com/",
     image: imgNoteTaker,
     description:
       "Challenge #11 for the University of Texas, Austin Coding Bootcamp 2023. The purpose of this challenge is to build a note taker application to save and retreive data from a JSON file.",
     technologies: "Node.js, Express.js",
-    GitHubRepo: "https://github.com/ibikunlefemi/note-taker",
+    GitHubRepo: "https://github.com/fibikunle/challenge11-note-taker",
   },
   {
     title: "Readme Generator",
-    url: "https://github.com/ibikunlefemi/readme-generator/",
+    url: "https://github.com/fibikunle/README-generator",
     image: imgReadmeGenerator,
     description:
       "Challenge #9 for the University of Texas, Austin Coding Bootcamp 2023. The purpose of this challenge is to build a GitHub README generator.",
     technologies: "Node.js & Inquirer v8.2.4",
-    GitHubRepo: "https://github.com/ibikunlefemi/readme-generator/",
+    GitHubRepo: "https://github.com/fibikunle/README-generator",
   },
 ];
